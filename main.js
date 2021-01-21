87dcbf0997aa1043292a35d7cd510036fc3a12d0
m-wellerson <m_wellerson@outlook.com>
2021-01-21 09:53:16 -0300

    Update

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-const width     = window.innerWidth;
-const height    = window.innerHeight;
-const renderer  = new THREE.WebGLRenderer({ antialias: true });
-const scene     = new THREE.Scene();
-const loader    = new THREE.ObjectLoader();
-const camera    = new THREE.PerspectiveCamera(75, width / height, 1, 1000);
+let logo         = document.querySelector('#rsw-logo');
+const width      = window.innerWidth;
+const height     = window.innerHeight;
+const renderer   = new THREE.WebGLRenderer({ antialias: true, canvas: logo });
+const scene      = new THREE.Scene();
+const loader     = new THREE.ObjectLoader();
+const camera     = new THREE.PerspectiveCamera(75, width / height, 1, 1000);
 const windowHalf = new THREE.Vector2( window.innerWidth / 2, window.innerHeight / 2 );
 const mouse      = new THREE.Vector2();
 const target     = new THREE.Vector2();
@@ -13,11 +14,11 @@ function onMouseMove( event ) {
 }
 function mouseSet(object) {
     const clock      = new THREE.Clock();
-    target.x = ( 1 - mouse.x ) * 0.002;
-    target.y = ( 1 - mouse.y ) * 0.002;
-    
-    object.rotation.x += 0.01 * ( target.y - object.rotation.x );
-    object.rotation.y += 0.01 * ( target.x - .1 * clock.getDelta());
+    target.x = ( 1 + mouse.x ) * 0.002;
+    target.y = ( 1 + mouse.y ) * 0.002;
+
+    object.rotation.x += 0.02 * ( target.y - object.rotation.x );
+    object.rotation.y += 0.02 * ( target.x - .1 * clock.getDelta());
     document.addEventListener( 'mousemove', onMouseMove, false );
 }
 
@@ -35,14 +36,14 @@ window.addEventListener( 'resize', onResize, false );
 renderer.setSize(width, height);
 document.body.appendChild(renderer.domElement);
 
-// controls = new THREE.OrbitControls(
-//     camera, 
-//     renderer.domElement
-// );
-// controls.enableZoom    = false;
-// controls.minPolarAngle = Math.PI * 0.4;
-// controls.maxPolarAngle = Math.PI * 0.6;
-// controls.rotateSpeed   = 0.07;
+controls = new THREE.OrbitControls(
+    camera, 
+    renderer.domElement
+);
+controls.enableZoom    = false;
+controls.minPolarAngle = Math.PI * 0.4;
+controls.maxPolarAngle = Math.PI * 0.6;
+controls.rotateSpeed   = 0.07;
 
 loader.load("scene.json", function (object) {
     let clock      = new THREE.Clock();
